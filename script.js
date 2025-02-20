96553ac5213e1380b5a66bf644b242a2cb453aba
Divyansh Tulsiani <divyansh.tulsiani0@gmail.com>
2025-02-21 02:23:54 +0530

    Added howler js for background

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const weatherKey = "194292bb0f634338cecba7fe5f882312";
 function startbutton(){
   let inp = document.getElementById("inputbox1");
@@ -15,7 +20,13 @@ async function fetchdata(cityname) {
     const incomdata = await respone.json()
     console.log("success");
     let tem = incomdata.main.temp;
+    let temfeel = incomdata.main.feels_like;
+    let conditions = incomdata.weather[0].main
+    let conditions2 = incomdata.weather[0].description
+    let visible = incomdata.visibility
     console.log(tem);
+    let statevar = [tem,temfeel,conditions,conditions2,visible];
+    // document.getElementById("weatherdis").style.display = "flex";
   }
   catch{
 
